c0067e341df9a2b52fd45e9840e824efa5e3d263
buenorafa <rafaelbuenooo@outlook.com>
2025-02-03 15:37:33 -0300

    Adiciona router link para o Sign-in

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, RouterLink],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss',
 })
