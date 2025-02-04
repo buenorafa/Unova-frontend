1b09a3f5c828c4ddfc225de3c8abb432e0e9a902
buenorafa <rafaelbuenooo@outlook.com>
2025-02-04 11:02:56 -0300

    Adiciona rota p/ o componente profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,19 @@
 import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { ProfileComponent } from './profile/profile.component';
 import { NgModule } from '@angular/core';
 
 export const appRoutes: Routes = [
   { path: '', redirectTo: 'sign-in', pathMatch: 'full' }, // Redireciona para login ao iniciar
   { path: 'sign-in', component: SignInComponent },
   { path: 'sign-up', component: SignUpComponent },
+  { path: 'profile', component: ProfileComponent },
   { path: '**', redirectTo: 'sign-in' }, // Redireciona para login se a rota não existir
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
