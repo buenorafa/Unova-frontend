5bdb8b6ae9adcf939bdab5c337b3c109809d7e59
islanpedro01 <islan.figueiredo@academico.ifpb.edu.br>
2025-02-03 22:29:28 -0300

    Login done and sweet alert added

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { Router } from '@angular/router';
+import { UserService } from '../../shared/services/user.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, ReactiveFormsModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss',
 })
-export class SignInComponent {}
+export class SignInComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private userService: UserService, private roteador: Router) { 
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required]]
+    });
+
+  }
+
+  onSubmit() {
+    const { email, senha } = this.loginForm.value;
+    console.log('Email:', email);
+    this.userService.login(email, senha).subscribe({
+      next: (user) => {
+        console.log('Login bem-sucedido:', user);
+      },
+      error: (error) => {
+        console.error('Erro no login:', error);
+      }
+    });
+  }
+
+
+}
+
+
+
