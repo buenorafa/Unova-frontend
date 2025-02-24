65d788222b9f15e631c50f58923a60b875947485
Sammuel13 <sammluna13@gmail.com>
2025-02-23 21:27:47 -0300

    added product form

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,44 +1,74 @@
-import { Component } from '@angular/core';
-import { NavBarComponent } from '../nav-bar/nav-bar.component';
-import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
-import {ProductService} from "../../shared/services/product.service";
+import {Component, signal} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {ProductService} from '../../shared/services/product.service';
+import {SweetAlertService} from '../../shared/services/sweet-alert.service';
+import {Product} from '../../shared/models/product';
+import {FooterComponent} from "../footer/footer.component";
+import {NavBarComponent} from "../nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-add-product',
   standalone: true,
-  imports: [NavBarComponent, ReactiveFormsModule, FormsModule],
+  imports: [ReactiveFormsModule, FooterComponent, NavBarComponent],
   templateUrl: './add-product.component.html',
   styleUrl: './add-product.component.scss',
 })
 export class AddProductComponent {
-  productForm: FormGroup;
-  selectedFile!: File;
 
-  constructor(private fb: FormBuilder, private productService: ProductService) {
+  hide = signal(true);
+  productForm: FormGroup;
+  product = new Product();
+  selectedFile: File | null = null;
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private productService: ProductService,
+    private sweetAlert: SweetAlertService
+  ) {
     this.productForm = this.fb.group({
-      name: [''],
-      description: [''],
-      price: [''],
-      quantity: [''],
-      isActive: [true]
+      name: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+      price: ['', [Validators.required, Validators.min(0)]],
+      quantity: ['', [Validators.required, Validators.min(1)]],
+      isActive: [true],
     });
   }
 
-  onFileSelected(event: any) {
-    this.selectedFile = event.target.files[0];
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.selectedFile = input.files[0];
+    }
   }
 
   onSubmit() {
-    const formData = new FormData();
-    formData.append('name', this.productForm.get('name')?.value);
-    formData.append('description', this.productForm.get('description')?.value);
-    formData.append('price', this.productForm.get('price')?.value);
-    formData.append('quantity', this.productForm.get('quantity')?.value);
-    formData.append('isActive', this.productForm.get('isActive')?.value);
-    formData.append('image', this.selectedFile);
+    if (this.productForm.valid && this.selectedFile) {
+      this.product.name = this.productForm.get('name')!.value;
+      this.product.description = this.productForm.get('description')!.value;
+      this.product.price = this.productForm.get('price')!.value;
+      this.product.quantity = this.productForm.get('quantity')!.value;
+      this.product.isActive = this.productForm.get('isActive')!.value;
 
-    this.productService.addProduct(formData).subscribe(() => {
-      alert('Produto cadastrado com sucesso!');
-    });
+      const formData = new FormData();
+      formData.append('name', this.product.name);
+      formData.append('description', this.product.description);
+      formData.append('price', this.product.price.toString());
+      formData.append('quantity', this.product.quantity.toString());
+      formData.append('isActive', this.product.isActive.toString());
+      formData.append('image', this.selectedFile);
+
+      this.productService.addProduct(formData).subscribe({
+        next: () => {
+          this.sweetAlert.sucesso('Produto cadastrado com sucesso!');
+          this.router.navigate(['/store']);
+        },
+        error: (err) => {
+          this.sweetAlert.erro('Erro ao cadastrar produto!');
+          console.error('Erro no cadastro:', err);
+        }
+      });
+    }
   }
 }
--- src/shared/services/product.service.ts
+++ src/shared/services/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 export interface Product {
   id?: number;
@@ -24,7 +24,19 @@ export class ProductService {
     return this.http.get<Product[]>(this.apiUrl);
   }
 
+  getProductById(id: number): Observable<Product> {
+    return this.http.get<Product>(`${this.apiUrl}/${id}`);
+  }
+
   addProduct(product: FormData): Observable<Product> {
     return this.http.post<Product>(this.apiUrl, product);
+  }
+
+  updateProduct(id: number, product: FormData): Observable<Product> {
+    return this.http.put<Product>(`${this.apiUrl}/${id}`, product);
+  }
+
+  deleteProduct(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
 }
