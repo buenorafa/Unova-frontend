46ee039b6f94989d924b3ffd38e2a6e363b249a4
islanpedro01 <islanpedro.12@hotmail.com>
2025-02-25 12:14:42 -0300

    Getting products from db

--- src/app/profile/my-products/my-products.component.ts
+++ src/app/profile/my-products/my-products.component.ts
@@ -1,5 +1,8 @@
 import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { Product } from '../../../shared/models/product';
+import { ProductService } from '../../../shared/services/product.service';
+
 
 @Component({
   selector: 'app-my-products',
@@ -10,28 +13,52 @@ import { Component } from '@angular/core';
 })
 export class MyProductsComponent {
 
-  products = [
-    {
-      id: 1,
-      image: '/assets/imgs/pokemon-box1.png',
-      name: 'COPAG Box Cartas Pokémon Treinador Avançado Obsidiana em Chamas',
-      price: 'R$ 359,90',
-      quantity: 1,
-    },
-    {
-      id: 2,
-      image: '/assets/imgs/pokemon-box2.png',
-      name: 'Box Cards Pokémon Deck Batalha De Liga Gardevoir Ex - Copa',
-      price: 'R$ 159,90',
-      quantity: 2,
-    },
-    {
-      id: 3,
-      image: '/assets/imgs/pokemon-box3.png',
-      name: 'Pokémon TCG: Box Coleção com Miniatura - Tapu Koko',
-      price: 'R$ 157,37',
-      quantity: 1,
-    },
-  ];
+products:Product [] = []
 
-}
+  constructor(private productService:ProductService){
+    this.productService.getProducts().subscribe({
+      next: (produtos) => { this.products = produtos
+      },
+      error:(error) =>{
+        console.error('Erro ao carregar produtos:', error);
+      }
+    })
+
+    }
+
+    
+    getFullImageUrl(imageUrl: string | null | undefined): string {
+      if (!imageUrl) {
+        return 'assets/default-image.jpg';
+      }
+      return `http://localhost:8080${imageUrl}`;
+    }
+
+
+  }
+
+  // products = [
+  //   {
+  //     id: 1,
+  //     image: '/assets/imgs/pokemon-box1.png',
+  //     name: 'COPAG Box Cartas Pokémon Treinador Avançado Obsidiana em Chamas',
+  //     price: 'R$ 359,90',
+  //     quantity: 1,
+  //   },
+  //   {
+  //     id: 2,
+  //     image: '/assets/imgs/pokemon-box2.png',
+  //     name: 'Box Cards Pokémon Deck Batalha De Liga Gardevoir Ex - Copa',
+  //     price: 'R$ 159,90',
+  //     quantity: 2,
+  //   },
+  //   {
+  //     id: 3,
+  //     image: '/assets/imgs/pokemon-box3.png',
+  //     name: 'Pokémon TCG: Box Coleção com Miniatura - Tapu Koko',
+  //     price: 'R$ 157,37',
+  //     quantity: 1,
+  //   },
+  // ];
+
+// }
